5faa2a701cbf11f1fbfa31682acb7e5cb4222de7
Leong Peck Yoke <1647747+peckyoke@users.noreply.github.com>
2023-05-14 18:45:59 +0800

    Update test.php

--- src/test.php
+++ src/test.php
@@ -4,12 +4,12 @@ include 'paynowqr.php';
 include 'qrcode.php';
 
 $opts = (object)array(
-	'uen' => '201512004RACC', //Required: UEN of company
+	'uen' => 'myuen', //Required: UEN of company
     'amount' => 50.50,             //Specify amount of money to pay.
     'editable' => false,           //Whether or not to allow editing of payment amount. Defaults to false if amount is specified
     // 'expiry' => '20201231',       //Set an expiry date for the Paynow QR code (YYYYMMDD). If ommitted, defaults to 5 years from now.
     'refNumber' => 'GIT-INV-10001',  //Reference number for Paynow Transaction. Useful if you need to track payments for recouncilation.
-    'company' =>  'Asian Pastoral Institute' //Company name to embed in the QR code. Optional.               
+    'company' =>  '' //Company name to embed in the QR code. Optional.               
 );
 
 $qr = new PaynowQR\PaynowQR($opts);
